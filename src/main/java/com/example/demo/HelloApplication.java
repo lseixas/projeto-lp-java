2bf724dbab7302b00e7d2752f497c7db7368491b
lseixas <lseixas.iorio@gmail.com>
2025-05-08 16:15:47 -0300

    tralalero tralala

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -11,13 +11,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader2 = new FXMLLoader(
-                HelloApplication.class.getResource("calendar-view.fxml")
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("views/mainpage-view.fxml")
         );
-        Scene scene2 = new Scene(fxmlLoader2.load(), 500, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
 
         stage.setTitle("Hello!");
-        stage.setScene(scene2);
+        stage.setScene(scene);
         stage.show();
 
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,4 +13,6 @@ module com.example.demo {
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+    exports com.example.demo.controllers;
+    opens com.example.demo.controllers to javafx.fxml;
 }
