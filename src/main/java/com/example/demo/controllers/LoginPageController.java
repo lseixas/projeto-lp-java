2d3ecde54df886f3131417a90a933ced177f5ace
lseixas <lseixas.iorio@gmail.com>
2025-05-20 11:10:23 -0300

    added shit

--- src/main/java/com/example/demo/controllers/LoginPageController.java
+++ src/main/java/com/example/demo/controllers/LoginPageController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class LoginPageController {
@@ -20,64 +25,61 @@ public class LoginPageController {
     @FXML
     private Button cpfLoginButton;
 
-    private boolean extraLoaded = false;
+    private boolean loadedPasswordField = false;
 
     //handler for enter pressed
 
     public void handleEnterPressed(){
-
-        String cpf = cpfTextField.getText();
-        System.out.println("CPF inserido: " + cpf);
-        handleLoginButtonClick();
-
+        handleLogin();
     }
 
     //handler for button click
 
     public void handleLoginButtonClick(){
-
-        String cpf = cpfTextField.getText();
-        if(extraLoaded){
-            String password = passwordTextField.getText();
-            logicCpfPasswordInsert();
-        }
-        else {
-            logicCpfInsert();
-        }
+        handleLogin();
     }
 
-    //logic for cpf + password insertion
+    //handle login
 
-    public void logicCpfPasswordInsert(){
+    public void handleLogin(){
         String cpf = cpfTextField.getText();
         String password = passwordTextField.getText();
 
-        if (validateCpf(cpf) && validadePassword(password)){
-            System.out.println("CPF e senha válidos: " + cpf + ", " + password);
-        } else {
-            System.out.println("CPF ou senha inválidos: " + cpf + ", " + password);
+        if(!cpf.isEmpty() && password.isEmpty() ){
+            if(validateCpf(cpf)){
+                System.out.println("CPF válido: " + cpf);
+                addExtraInputPane();
+                loadedPasswordField = true;
+            } else {
+                System.out.println("CPF inválido: " + cpf);
+                //show error message
+            }
         }
-    }
+        if(!cpf.isEmpty() && !password.isEmpty()){
+            if(validateCpf(cpf) && validadePassword(password)){
+                System.out.println("CPF e senha válidos: " + cpf + ", " + password);
+                //proceed to next page
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/mainPage/mainPage-view.fxml"));
+                    Parent root = loader.load();
 
-    //logic for first insertion
-
-    public void logicCpfInsert(){
-
-        String cpf = cpfTextField.getText();
-
-        if (validateCpf(cpf)){
-
-            System.out.println("CPF válido: " + cpf);
-            addExtraInputPane();
-            extraLoaded = true;
-
-            String password = passwordTextField.getText();
-            System.out.println("Senha inserida: " + password);
-
-        } else {
+                    // get current stage from any node
+                    Stage stage = (Stage) cpfTextField.getScene().getWindow();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println("CPF ou senha inválidos: " + cpf + ", " + password);
+                //show error message
+            }
+        }
+        if (cpf.isEmpty()){
             System.out.println("CPF inválido: " + cpf);
+            //show error message
         }
-
     }
 
     //visible password text field
--- src/main/java/com/example/demo/controllers/MainPageController.java
+++ src/main/java/com/example/demo/controllers/MainPageController.java
@@ -1,4 +1,17 @@
 package com.example.demo.controllers;
 
 public class MainPageController {
+
+    public void buttonConta() {
+
+        //change to conta screen
+
+    }
+
+    public void buttonTransacao() {
+
+        //change to transacao screen
+
+    }
+
 }
