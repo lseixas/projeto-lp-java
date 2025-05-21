ac508fe996421e82808112a14307a5080051b6b2
lseixas <lseixas.iorio@gmail.com>
2025-05-21 15:26:01 -0300

    quick style to login page and top bar, created simple createAccountPage

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("views/loginPage-view.fxml")
+                HelloApplication.class.getResource("views/createAccountPage-view.fxml")
         );
         Scene scene = new Scene(fxmlLoader.load(), 500, 	500);
 
