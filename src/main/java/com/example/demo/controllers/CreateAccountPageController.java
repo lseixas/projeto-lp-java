7582ed9fc0e88bf6097a00dff7bc4fc9c1833c6f
lseixas <lseixas.iorio@gmail.com>
2025-05-21 17:01:26 -0300

    added text fields to create acc page controller, moved on key pressed to whole stack pane in loginPage view

--- src/main/java/com/example/demo/controllers/CreateAccountPageController.java
+++ src/main/java/com/example/demo/controllers/CreateAccountPageController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CreateAccountPageController {
 
+    @FXML
+    public TextField cAccNameField;
+    @FXML
+    public TextField cAccEmailField;
+    @FXML
+    public TextField cAccBirthField;
+    @FXML
+    public TextField cAccCpfField;
+    @FXML
+    public TextField cAccPasswordField;
+    @FXML
+    public TextField cAccConfirmPasswordField;
+
+    public void initialize() {
+        // Initialization logic if needed
+    }
 }
