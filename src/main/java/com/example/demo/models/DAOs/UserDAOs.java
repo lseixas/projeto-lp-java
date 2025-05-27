86da15bde0a4cb6c23f7882162d68b4732b1efa0
lseixas <lseixas.iorio@gmail.com>
2025-05-27 17:03:17 -0300

    readded cpf and email queries

--- src/main/java/com/example/demo/models/DAOs/UserDAOs.java
+++ src/main/java/com/example/demo/models/DAOs/UserDAOs.java
@@ -24,4 +24,48 @@ public class UserDAOs {
             stm.executeUpdate();
         }
     }
+
+    public User getUserByCpf(Connection conn, String cpf) throws SQLException {
+        String sql = "SELECT * FROM `banco_cvetti_users`.`usuario` WHERE `cpf` = ?;";
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+            stm.setString(1, cpf);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                UUID userId = UUID.fromString(rs.getString("id"));
+                String nome = rs.getString("nome");
+                String email = rs.getString("email");
+                String senha = rs.getString("senha");
+                float saldo = rs.getFloat("saldo");
+                java.sql.Date nascimento = rs.getDate("nascimento");
+
+                User user = new User(nome, email, cpf, senha, saldo, nascimento);
+
+                return user;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public User getUserByEmail(Connection conn, String email) throws SQLException {
+        String sql = "SELECT * FROM `banco_cvetti_users`.`usuario` WHERE `email` = ?;";
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+            stm.setString(1, email);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                UUID userId = UUID.fromString(rs.getString("id"));
+                String nome = rs.getString("nome");
+                String cpf = rs.getString("cpf");
+                String senha = rs.getString("senha");
+                float saldo = rs.getFloat("saldo");
+                java.sql.Date nascimento = rs.getDate("nascimento");
+
+                User user = new User(nome, email, cpf, senha, saldo, nascimento);
+
+                return user;
+            } else {
+                return null;
+            }
+        }
+    }
 }
