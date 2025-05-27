8c0523d90598471bceb209bc4e5279c2d70c20a1
Arthur199S <arthursilvacorreia199@gmail.com>
2025-05-27 10:31:40 -0300

    Correçao de problemas+logo(IA sem problemas de direitos autorais)+imagem volta para inicio (#5)
    * refactor: change layout from VBox to HBox for top bar
    
    * mudanças
    
    ---------
    
    Co-authored-by: Arthur199S <23.00877-6@maua.br>

--- src/main/java/com/example/demo/controllers/TopBarController.java
+++ src/main/java/com/example/demo/controllers/TopBarController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controllers;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class TopBarController {
+
+    public void handleLogoClick(MouseEvent event) {
+        try {
+            // Carrega o FXML da tela de login
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/views/loginPage-view.fxml"));
+            // Obtém o Stage atual a partir do evento
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            // Troca a cena
+            stage.setScene(new Scene(root, 500, 500));
+        } catch (IOException e) {
+        }
+    }
 }
