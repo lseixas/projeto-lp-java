54eb4c150e875957120e06af7456f01f5f5b3b84
lseixas <lseixas.iorio@gmail.com>
2025-06-09 13:18:40 -0300

    added rds.tf

---
+++ rds.tf
@@ -0,0 +1,16 @@
+#create our rds DB using TF
+
+resource "aws_db_instance" "myrds" {
+  engine              = "mysql"
+  engine_version      = "8.0.27"
+  allocated_storage   = 1
+  storage_type        = "gp2"
+  identifier          = "mydb"
+  username            = "root"
+  password            = "root"
+  publicly_accessible = true
+  skip_final_snapshot = true
+  instance_class      = "db.t3.micro"
+  db_name             = "aws_banco_cvetti"
+
+}
